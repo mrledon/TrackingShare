431c76734bf185d43ac89418140ad42ea5035132
ChuanNN <chuannn@dolphinsolutions.vn>
2018-11-08 15:57:12 +0700

    Update SRC

---
+++ SourceCode/EmployeeTracking/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeTracking.Controllers
+{
+    public class AccountController : BasicController
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+
+            return RedirectToAction("Index","Home");
+        }
+
+        
+
+    }
+}
