e325726a625ea24a3d43e0f9b73aa36b3a7dfe5a
Lê Văn Đôn <mrledon90@hotmail.com>
2018-11-27 01:43:50 +0000

    Initail Static function

---
+++ SourceCode/EmployeeTracking.Admin/Controllers/StatisticController.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeTracking.Admin.App_Helper;
+using EmployeeTracking.Core.Repositories;
+using EmployeeTracking.Data.ModelCustom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeTracking.Core;
+
+namespace EmployeeTracking.Admin.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private StoreRepo _storeRepo;
+
+        public StatisticController()
+        {
+            _storeRepo = new StoreRepo();
+        }
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ SourceCode/EmployeeTracking.Core/Repositories/StatisticRepo.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeTracking.Data.CommonData;
+using EmployeeTracking.Data.Database;
+using EmployeeTracking.Data.ModelCustom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTracking.Core.Repositories
+{
+    class StatisticRepo
+    {
+        public List<StatisticNumberStoreDay> getStoreNumber5Days()
+        {
+            using (employeetracking_devEntities _data = new employeetracking_devEntities())
+            {
+                var ls5Days = new List<StatisticNumberStoreDay>();
+                for (var day = DateTime.Now.AddDays(-4); day.Date <= DateTime.Now; day = day.AddDays(1))
+                {
+                    ls5Days.Add(new StatisticNumberStoreDay() { storeFail = 9, storeSucesss = 99, storeUnSubmit = 50 });
+                }
+
+                return ls5Days;
+            }
+        }
+    }
+
+    public class StatisticNumberStoreDay
+    {
+        public int storeSucesss { get; set; }
+        public int storeFail { get; set; }
+        public int storeUnSubmit { get; set; }
+    }
+}
