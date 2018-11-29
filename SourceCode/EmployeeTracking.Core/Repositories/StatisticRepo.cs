6c84b391e60065aba80f7b0ccf40bbbe5c1cf371
Anthony <dinhhung09138@gmail.com>
2018-11-29 09:37:22 +0700

    Check trường hợp hiển thị số lượng của hàng chưa gởi báo cáo

--- SourceCode/EmployeeTracking.Core/Repositories/StatisticRepo.cs
+++ SourceCode/EmployeeTracking.Core/Repositories/StatisticRepo.cs
@@ -24,29 +24,18 @@ namespace EmployeeTracking.Core.Repositories
                 {
                     //Get end of day
                     var endofday = day.AddDays(1).AddSeconds(-1);
-                    
+
                     //Get data curent
                     var Todaytrack = db.tracks.Where(x => x.CreateDate >= day && x.CreateDate <= endofday);
-                    
+
                     var storeFail = (from tr in Todaytrack where tr.StoreStatus == false select tr).Count();
 
                     var storeSuccess = (from ms in (from subms in db.master_store join tr in Todaytrack on subms.Id equals tr.MasterStoreId where tr.StoreStatus == true select subms) select ms).Count();
-
-                    var UnSubmitTrack = db.tracks.Where(x => db.track_session
-                                                        .Join(db.tracks,
-                                                        ts => ts.TrackId, td => td.Id,
-                                                        (ts, td) => new { TS = ts, TD = td.Id })
-                                                        .Where(y => y.TS.TrackId == x.Id).Count() == 0);
-
-                    //var UnSubmitTrack = (from ts in db.track_session
-                    //                     join t in db.tracks on ts.TrackId equals t.Id
-                    //                     where t.Date >= day && t.Date <= endofday && !(bool)ts.Status
-                    //                     select new { t.MasterStoreId }).Distinct().ToList();
-
-                    var storeUnSubmit = (from ms in (from subms in db.master_store
-                                                     join tr in UnSubmitTrack on subms.Id equals tr.MasterStoreId
-                                                     select subms)
-                                         select ms).Count();
+                    
+                    var storeUnSubmit = (from tr in db.tracks
+                                         join ts in db.track_session on tr.Id equals ts.TrackId
+                                         where tr.Date >= day && tr.Date <= endofday && !(bool)ts.Status
+                                         select new { tr.MasterStoreId }).Distinct().ToList().Count();
 
                     ls5Days.Add(new StatisticNumberStoreDay()
                     {
@@ -61,7 +50,7 @@ namespace EmployeeTracking.Core.Repositories
                     day = day.AddDays(1);
 
                 } while (day <= DateTime.Now);
-                
+
                 return ls5Days;
             }
         }
@@ -90,11 +79,11 @@ namespace EmployeeTracking.Core.Repositories
                 {
                     model.ProvinceName = data.provinces.FirstOrDefault(x => x.Id == masterStore.ProvinceId).Name;
                 }
-                    if(data.districts.FirstOrDefault(x => x.Id == masterStore.DistrictId)!=null)
+                if (data.districts.FirstOrDefault(x => x.Id == masterStore.DistrictId) != null)
                 {
                     model.DistrictName = data.districts.FirstOrDefault(x => x.Id == masterStore.DistrictId).Name;
                 }
-                if (data.wards.FirstOrDefault(x => x.Id == masterStore.WardId)!=null)
+                if (data.wards.FirstOrDefault(x => x.Id == masterStore.WardId) != null)
                 {
                     model.WardName = data.wards.FirstOrDefault(x => x.Id == masterStore.WardId).Name;
                 }
