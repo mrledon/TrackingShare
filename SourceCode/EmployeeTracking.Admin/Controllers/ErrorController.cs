03be01ff85f1644bf8d1c11a587e636cd661a843
Anthony <dinhhung09138@gmail.com>
2018-12-10 22:52:08 +0700

    Các màn hình sẽ hiển thị khi xử lý phân quyền, lỗi

---
+++ SourceCode/EmployeeTracking.Admin/Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeTracking.Admin.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Forbiden()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
