af8f463d6b5927046010a7ba832a72b14404b160
Pham Trung Hieu <hieup0363@gmail.com>
2018-12-20 22:19:07 +0700

    Thêm phần filter trạng thái submit

--- SourceCode/EmployeeTracking.Data/ModelCustom/ImageManagementViewModel.cs
+++ SourceCode/EmployeeTracking.Data/ModelCustom/ImageManagementViewModel.cs
@@ -71,5 +71,6 @@ namespace EmployeeTracking.Data.ModelCustom
         /// </summary>
         public IEnumerable<TrackSessionViewModel> TrackSessions { get; set; }
         public List<string> lstRole { get; set; }
+        public int TrackSessionStatus { get; set; } = -1;
     }
 }
